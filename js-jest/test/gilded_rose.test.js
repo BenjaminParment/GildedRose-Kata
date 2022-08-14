3fd29b1f155091ab0c1cbff5692addb5bd1b6835
BenjaminParment <parment.benjamin@gmail.com>
2022-08-14 12:35:13 +0200

    Adds the sulfuras test suite

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -104,41 +104,33 @@ describe("Gilded Rose", () => {
     });
   });
 
-  // // LEGENDARY SULFURAS
-  // describe("Given the aged brie article", () => {
-  //   beforeEach(() => (item.name = AGED_BRIE));
+  // LEGENDARY SULFURAS
+  describe("Given the sulfuras article", () => {
+    beforeEach(() => (item.name = SULFURAS));
 
-  //   describe("When its sellin is above 0", () => {
-  //     beforeEach(() => (item.sellIn = 10));
+    describe("When its sellin is above 0", () => {
+      beforeEach(() => (item.sellIn = 10));
 
-  //     it("Should increase quality and decrease sellin by 1 on update", () => {
-  //       item.quality = 10;
-  //       gildedRose = new Shop([item]);
-  //       items = gildedRose.updateQuality();
-  //       expect(items[0].sellIn).toBe(9);
-  //       expect(items[0].quality).toBe(11);
-  //     });
+      it("Should not change the quality nor sellin on updates", () => {
+        item.quality = 10;
+        gildedRose = new Shop([item]);
+        items = gildedRose.updateQuality();
+        expect(items[0].sellIn).toBe(10);
+        expect(items[0].quality).toBe(10);
+      });
+    });
 
-  //     it("Should not increase quality if set at 50", () => {
-  //       item.quality = 50;
-  //       gildedRose = new Shop([item]);
-  //       items = gildedRose.updateQuality();
-  //       expect(items[0].sellIn).toBe(9);
-  //       expect(items[0].quality).toBe(50);
-  //     });
-  //   });
+    describe("When its sellin is set to 0 or less", () => {
+      beforeEach(() => ((item.sellIn = -10), (item.quality = 10)));
 
-  //   describe("When its sellin is set to 0 or less", () => {
-  //     beforeEach(() => ((item.sellIn = 0), (item.quality = 10)));
-
-  //     it("Should decrement sellin on updates while increasing the quality by two", () => {
-  //       gildedRose = new Shop([item]);
-  //       items = gildedRose.updateQuality();
-  //       expect(items[0].sellIn).toBe(-1);
-  //       expect(items[0].quality).toBe(12);
-  //     });
-  //   });
-  // });
+      it("Should not change any properties on updates", () => {
+        gildedRose = new Shop([item]);
+        items = gildedRose.updateQuality();
+        expect(items[0].sellIn).toBe(-10);
+        expect(items[0].quality).toBe(10);
+      });
+    });
+  });
 
   // // RANDOM ITEM
   // describe("Given the aged brie article", () => {
