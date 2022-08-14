bd000b0e01f83b419d84b820ab700ec94f809632
BenjaminParment <parment.benjamin@gmail.com>
2022-08-14 12:14:30 +0200

    Adds test suite for Aged brie articles

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -2,10 +2,45 @@ const { Shop, Item } = require("../src/gilded_rose");
 const { AGED_BRIE, BACKSTAGE_PASS, SULFURAS } = require("../const/index.js");
 
 describe("Gilded Rose", () => {
-  it("should update aged brie correctly", () => {
-    const gildedRose = new Shop([new Item(AGED_BRIE, 10, 10)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].sellIn).toBe(9);
-    expect(items[0].quality).toBe(11);
+  let item;
+
+  beforeEach(() => {
+    item = new Item(null, null, 10);
+  });
+
+  // AGED BRIE
+  describe("Given the aged brie article", () => {
+    beforeEach(() => (item.name = AGED_BRIE));
+
+    describe("When its sellin is above 0", () => {
+      beforeEach(() => (item.sellIn = 10));
+
+      it("Should increase quality and decrease sellin by 1 on update", () => {
+        item.quality = 10;
+        const gildedRose = new Shop([item]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).toBe(9);
+        expect(items[0].quality).toBe(11);
+      });
+
+      it("Should not increase quality if set at 50", () => {
+        item.quality = 50;
+        const gildedRose = new Shop([item]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).toBe(9);
+        expect(items[0].quality).toBe(50);
+      });
+    });
+
+    describe("When its sellin is set to 0 or less", () => {
+      beforeEach(() => ((item.sellIn = 0), (item.quality = 10)));
+
+      it("Should decrement sellin on updates while increasing the quality by two", () => {
+        const gildedRose = new Shop([item]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).toBe(-1);
+        expect(items[0].quality).toBe(12);
+      });
+    });
   });
 });
